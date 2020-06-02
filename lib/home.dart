1194c29b5ff1317d08626374cbbe877cd37f85eb
Shubham Garg <shubham8garg@gmail.com>
2020-06-02 18:37:02 -0400

    Shared Preferences, Name and Icon

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Home extends StatefulWidget {
 
@@ -9,12 +10,15 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
 
-  List<String> names = [
-    "Aaloo Tamatar",
-    "Maggi"
-  ];
-
+  List<String> dinners = new List<String>();
+  SharedPreferences sharedPreferences;
   TextEditingController nameController = TextEditingController();
+
+  @override
+  void initState(){ 
+    loadSharedPreferencesAndData();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,21 +33,109 @@ class _HomeState extends State<Home> {
         centerTitle: true,
         backgroundColor: Colors.white,
       ),
-      body: Column(
+      body: dinners.isEmpty ? emptyList() : buildListItems(),
+    );
+  }
+
+  void loadSharedPreferencesAndData() async {
+    sharedPreferences = await SharedPreferences.getInstance();
+    loadData();
+  }
+
+  void loadData() {
+    List<String> dinnerlist = sharedPreferences.getStringList('dinners') ?? [];
+    dinners = dinnerlist;
+    if(dinnerlist != null){
+      setState((){
+        dinners = dinnerlist;
+      });
+    }
+  }
+
+  void addToList(){
+    if (nameController.text.isNotEmpty) {
+      dinners.add(nameController.text);
+      sharedPreferences.setStringList('dinners', dinners);
+      setState(() {
+        dinners.add(nameController.text);
+        loadData();
+      });
+    }
+  }
+  void saveListAfterRemoving(){
+    sharedPreferences.setStringList('dinners', dinners);
+    setState(() {
+      loadData();
+    });
+  }
+
+  Widget emptyList(){
+    return Column(
+            children: [
+              Expanded(
+                child:  Center(child: Text('No items'))
+              ),
+              Expanded(
+                child: Padding(
+                  padding: EdgeInsets.all(20.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Text(
+                        "Add New Name",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.w800,
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: TextField(
+                              controller: nameController,
+                              decoration: InputDecoration(
+                                contentPadding: EdgeInsets.only(top: 10, bottom: 10),
+                                isDense: true,
+                              )
+                            )
+                          ),
+                          RaisedButton(
+                            child: Text("Add"),
+                            onPressed: () {
+                              addToList();
+                              setState(() {
+                                nameController.clear();
+                              });
+                            }
+                            )
+                        ]
+                      ),
+                    ],
+                  ),
+                  ),
+                )
+            ]
+    );
+  }
+
+  Widget buildListItems(){
+    return Column(
         children: <Widget>[
           Expanded(
             child: ListView.builder(
-              itemCount: names.length,
+              itemCount: dinners.length,
               itemBuilder: (context, index) {
                 return Dismissible(
                   key: UniqueKey(), 
                   onDismissed: (direction) {
                     setState(() {
-                      names.removeAt(index);
+                      dinners.removeAt(index);
                     });
+                    saveListAfterRemoving();
                   },
                   child: ListTile(
-                    title: Text(names[index]),
+                    title: Text(dinners[index]),
                   )
                   );
               }
@@ -92,7 +184,7 @@ class _HomeState extends State<Home> {
                         var random = Random();
                         Scaffold.of(context).showSnackBar(
                           SnackBar(
-                            content: Text("Random Dinner is: "+ names[random.nextInt(names.length)]),
+                            content: Text("Random Dinner is: "+ dinners[random.nextInt(dinners.length)]),
                             )
                         );
                       },
@@ -103,14 +195,6 @@ class _HomeState extends State<Home> {
               ),
             )
         ]
-      ),
-    );
-  }
-  void addToList() {
-    if (nameController.text.isNotEmpty) {
-      setState(() {
-        names.add(nameController.text);
-      });
-    }
+      );
   }
 }
